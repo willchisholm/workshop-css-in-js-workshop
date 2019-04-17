55e46ab9a444e6e79d1e94fb6146191943401686
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-04-17 17:11:39 -0400

    done with demo

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyBbQhanGbjbZ_rtTht3EX-Z8PeRgDmGjwc'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
